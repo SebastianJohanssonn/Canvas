d4035838139a10cfd77c245ccd0597e5734500ac
Sebastian Johansson <sebben.97@hotmail.com>
2018-10-29 12:01:43 +0100

    Drawed one dot.

--- draw.js
+++ draw.js
@@ -28,3 +28,20 @@ function drawCircle(context){
     context.arc(circleRadius + offset, circleRadius + offset, circleRadius, 0, 2 * Math.PI);
     context.stroke();
 }
+
+/**
+ * Draws the dots for this project.
+ * @param {CanvasRenderingContext2D} context
+ */
+
+function drawDots(context){
+    var outlineCircleCenter = circleRadius + offset;
+    var x = outlineCircleCenter + circleRadius * Math.cos(0);
+    var y = outlineCircleCenter + circleRadius * Math.sin(0);
+    
+    context.beginPath();
+    context.lineWidth = lineWidth;
+    context.fillStyle = "black";
+    context.arc(x, y, dotRadius, 0, 2 * Math.PI);
+    context.fill();
+ }
